fb0bf0d692d0f48649ee283ad681b97780b4864a
Vitor Jesus <vitorjdev@gmail.com>
2023-09-25 21:30:27 -0300

    feat: initial commit

---
+++ DotNet-AzureKeyVault_Certificates/ConsoleReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_AzureKeyVault_Certificates
+{
+    public static class ConsoleReader
+    {
+        public static Uri GetUrl()
+        {
+            Console.WriteLine("Insert the Key Vault url..");
+            var url = ForceReadLine("Enter a valid url...");
+            return new Uri(url);
+        }
+
+        private static string ForceReadLine(string? retryMessage = null)
+        {
+            string? content = null;
+
+            do
+            {
+                content = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(retryMessage) && string.IsNullOrWhiteSpace(content))
+                {
+                    Console.WriteLine(retryMessage);
+                }
+
+            } while (string.IsNullOrWhiteSpace(content));
+
+            return content;
+        }
+    }
+}
---
+++ DotNet-AzureKeyVault_Certificates/Program.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Certificates;
+using DotNet_AzureKeyVault_Certificates;
+
+var clientUrl = ConsoleReader.GetUrl();
+var client = new CertificateClient(clientUrl, new DefaultAzureCredential());
+
+Console.WriteLine("Listing certificates...");
+var certificates = client.GetPropertiesOfCertificates();
+if (!certificates.Any())
+{
+    Console.WriteLine("This Vault don't have certificates");
+    return;
+}
+
+foreach (var certificate in certificates)
+{
+    Console.WriteLine($"Name {certificate.Name}, Id {certificate.Id}");
+}
